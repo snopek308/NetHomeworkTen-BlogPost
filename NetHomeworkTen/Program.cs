3db4a171712a3fecc8cf7b560ce4136462d2a2db
asnopek <asnopek486@gmail.com>
2020-04-20 14:34:07 -0500

    Used NLogger, added in validation, changed around my switch

--- NetHomeworkTen/Program.cs
+++ NetHomeworkTen/Program.cs
@@ -12,6 +12,7 @@ namespace NetHomeworkTen
     {
         class MainClass
         {
+            //comment
             private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
             public static void Main(string[] args)
